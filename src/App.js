36a995d003a294fabafa4a9623e12425c8c7de38
vamshithati <vthati@hawk.iit.edu>
2025-02-07 14:04:13 -0600

    done with basic Form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from "./component/Home";
+
+ function App() {
+
+
+
+ 
+
+
+  return (
+    <div >
+  <h1>Expense Tracker</h1>
+  <Home/>
+  </div>
+)
+  ;
+ }
+export default App
