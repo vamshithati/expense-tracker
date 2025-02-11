842aa7acacfbe0fc125dca0745c22a0008d05199
vamshithati <vthati@hawk.iit.edu>
2025-02-10 23:49:18 -0600

    passing new methods

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,13 +5,25 @@ import { useState } from "react";
 const Home=()=>{
     const [transactions,setTransactions]=useState([]);
     const addtransactions = (amount, description) => {
-        setTransactions((prev) => [...prev, { amount, description }]);
+        setTransactions((prev) => [...prev, { amount: Number(amount), description }]);
     };
+
+    const deleteTransaction = (index) => {
+        const updatedTransactions = transactions.filter((_, i) => i !== index);
+        setTransactions(updatedTransactions);
+    };
+    const editTransaction = (index, newAmount, newDescription) => {
+        let updatedTransactions = [...transactions]; // Copy transactions
+        updatedTransactions[index] = { amount: Number(newAmount), description: newDescription }; // Update the selected one
+        setTransactions(updatedTransactions);
+    };
+
     return (
     <div >
     
-   <br/> <OverviewComponent addtransactions={addtransactions}/>
-    <br/><TransactionComponent transactions={transactions}/>
+   <br/> <OverviewComponent transactions={transactions} addtransactions={addtransactions}/>
+    <br/><TransactionComponent transactions={transactions} deleteTransaction={deleteTransaction}
+    editTransaction={editTransaction}/>
     </div>
 )
 };
